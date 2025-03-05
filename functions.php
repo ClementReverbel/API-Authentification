d54e7bd5df68d8d08e695f9c4268aa3e32e9f365
Salairedesalledebain <zyad.iscles@gmail.com>
2025-03-05 12:31:15 +0100

    problème noms colonnes

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
 // Vérifie si un utilisateur existe dans la base de données
 function userExist($login){
         $linkpdo = connexion_db();
-        $requete = $linkpdo->prepare("SELECT count(*) FROM utilisateurs WHERE login = :login");
+        $requete = $linkpdo->prepare("SELECT count(*) FROM utilisateur WHERE login = :login");
         $requete->execute(array('login'=>$login));
         $res = $requete->fetch();
         return $res[0] >= 1;
@@ -13,11 +13,11 @@ function userExist($login){
 // Récupère le mot de passe hashé d'un utilisateur
 function getPassword($login){
         $linkpdo = connexion_db();
-        $requete = $linkpdo->prepare("SELECT mdp FROM user WHERE login = :login");
+        $requete = $linkpdo->prepare("SELECT mdp FROM utilisateur WHERE login = :login");
         $requete->execute(array('login'=>$login));
         $res = $requete->fetch();
         if(!empty($res)){
-            return $res['password'];
+            return $res['mdp'];
         }
     }
 
